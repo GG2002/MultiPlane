c473b86f7b20274972baa59b1de2f1b1fb6d55f1
cyc <770161366@qq.com>
2020-08-10 22:49:05 +0800

    投影门禁可能完毕

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,66 @@
 window.onload = e => {
+    welcomeBGIn();
+    logAnimateIn();
+    $(".login").click(function () {
+        logAnimateOut();
+        welcomeBGOut();
+    })
+}
+window.onresize = e => {
+    let dbHeight = $(document.body).height();
+    let door_left = document.querySelectorAll('.door-left');
+    door_left.forEach((e, w) => {
+        e.style.top = w * dbHeight / 6 + 'px';
+    })
+    let door_right = document.querySelectorAll('.door-right');
+    door_right.forEach((e, w) => {
+        e.style.top = w * dbHeight / 6 + 'px';
+    })
+}
+function logAnimateIn() {
+    $("#log").animate({
+        opacity: "1",
+        border: '1px solid #ffffff',
+    }).animate({
+        width: "360px"
+    }).animate({
+        height: "360px",
+        padding: "60px 32px",
+    })
+    setTimeout(() => {
+        $("#logo").fadeIn(1000);
+        $(".log").fadeIn(1500);
+        $("#log-bg").fadeIn(2000);
+        $(".login").animate({
+            opacity: "1"
+        }, 2000);
+        $("#log").addClass("flash");
+    }, 1500)
+
+}
+function logAnimateOut() {
+    $("#log").animate({
+        opacity: "1",
+        height: "0px",
+        padding: "0"
+    }).animate({
+        width: "0"
+    }).animate({
+        border: '0'
+    })
+    $(".login").fadeOut()
+    $(".log").fadeOut()
+    $("#log-bg").animate({
+        opacity: "1",
+        height: "0px",
+        padding: "0"
+    }).animate({
+        width: "0"
+    }).animate({
+        border: '0'
+    })
+}
+function welcomeBGIn() {
     let pathEls = document.querySelectorAll('path');
     for (let i = 0; i < pathEls.length; i++) {
         let pathEl = pathEls[i];
@@ -16,16 +78,7 @@ window.onload = e => {
         });
     }
     let dbHeight = $(document.body).height();
-    console.log(dbHeight)
-    $("#log").addClass('rotateIn');
-    setTimeout(()=>{
-        $('#log').css('opacity',1);
-    },800)
     $("#welcome").css('height', dbHeight + 'px')
-    setTimeout(() => {
-        $("#log").removeClass('rotateIn')
-        $("#log").addClass('lightChange')
-    }, 1000);
     let door_left = document.querySelectorAll('.door-left');
     door_left.forEach((e, w) => {
         e.style.top = w * dbHeight / 6 + 'px';
@@ -33,36 +86,24 @@ window.onload = e => {
     let door_right = document.querySelectorAll('.door-right');
     door_right.forEach((e, w) => {
         e.style.top = w * dbHeight / 6 + 'px';
-    })
-    $(".login").click(function () {
-        $("#welcome-bg").removeClass("door-light-change").addClass("fade").fadeOut(1000);
-        $("#log").removeClass('lightChange').addClass('rotateOut').fadeOut(1200);
-        setTimeout(() => {
-            anime({
-                targets: '.door-right',
-                translateX: 580,
-                delay: anime.stagger(100) // 每个元素的延迟增加100毫秒。
-            });
-            anime({
-                targets: '.door-left',
-                translateX: -580,
-                delay: anime.stagger(100) // 每个元素的延迟增加100毫秒。
-            });
-            setTimeout(() => {
-                $("#welcome").fadeOut(2500);
-                $("#main").fadeIn(3000);
-            }, 700);
-        }, 1200)
     })
 }
-window.onresize = e => {
-    let dbHeight = $(document.body).height();
-    let door_left = document.querySelectorAll('.door-left');
-    door_left.forEach((e, w) => {
-        e.style.top = w * dbHeight / 6 + 'px';
-    })
-    let door_right = document.querySelectorAll('.door-right');
-    door_right.forEach((e, w) => {
-        e.style.top = w * dbHeight / 6 + 'px';
-    })
+function welcomeBGOut() {
+    $("#welcome-bg").removeClass("door-light-change").addClass("fade").fadeOut(1000);
+    setTimeout(() => {
+        anime({
+            targets: '.door-right',
+            translateX: 580,
+            delay: anime.stagger(100) // 每个元素的延迟增加100毫秒。
+        });
+        anime({
+            targets: '.door-left',
+            translateX: -580,
+            delay: anime.stagger(100) // 每个元素的延迟增加100毫秒。
+        });
+        setTimeout(() => {
+            $("#welcome").fadeOut(2500);
+            $("#main").fadeIn(3000);
+        }, 700);
+    }, 1200)
 }
